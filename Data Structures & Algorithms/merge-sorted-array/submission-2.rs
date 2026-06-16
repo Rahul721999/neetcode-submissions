77765d2159aa3ae67a58f841226d62f485d38089
Rahul721999 <109303988+Rahul721999@users.noreply.github.com>
2026-06-16 08:05:14 +0530

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        let mut p3 = m + n - 1;
+        let mut p2 = n - 1;
+        let mut p1 = m - 1;
+
+        while p1 >= 0 && p2 >= 0 {
+            if nums1[p1 as usize] >= nums2[p2 as usize]{
+                nums1[p3 as usize] = nums1[p1 as usize];
+                p1 -= 1;
+            }else{
+                nums1[p3 as usize] = nums2[p2 as usize];
+                p2 -= 1;
+            }
+            p3 -= 1;
+        }
+
+            while p2 >= 0 {
+                nums1[p3 as usize] = nums2[p2 as usize];
+                p2 -= 1;
+                p3 -= 1;
+            };
+
+    }
+}
